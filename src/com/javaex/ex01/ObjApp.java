3f9c10883a95bfc62bf5b02b11d51631f8afd6ff
wonho <wonho951@gmail.com>
2021-06-11 12:20:53 +0900

    2021.06.11 수업내용 기본API

--- src/com/javaex/ex01/ObjApp.java
+++ src/com/javaex/ex01/ObjApp.java
@@ -4,6 +4,20 @@ public class ObjApp {
 
 	public static void main(String[] args) {
 
+		/*
+		Object obj01 = new Object(); //디폴트 생성자가 있다는 소리
+		
+		System.out.println(obj01.toString());
+		*/
+		
+		
+		
+		//실험 - toString(Object의 toString 불러오기)
+		Point p01 = new Point(100, 100);
+		System.out.println(p01.toString()); //toSring 정의:: getClass().getName() + "@" + Integer.toHexString(hashCode())
+
+		
+		
 	}
 
 }

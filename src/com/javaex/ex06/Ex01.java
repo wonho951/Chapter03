d513391394aaf740d7bcc4fa71a775ca592e8583
wonho <wonho951@gmail.com>
2021-06-14 10:19:19 +0900

    2021.06.14 수업내용 wrapper class - integer

--- src/com/javaex/ex06/Ex01.java
+++ src/com/javaex/ex06/Ex01.java
@@ -8,7 +8,13 @@ public class Ex01 {
 		Integer i01 = new Integer(10);	//--> Integer 곧 자바에서 없어질듯
 		Integer i02 = new Integer(7);
 		
-		Integer result = i01 + i02;
+		
+		
+		//int result = 10 + 7;	-->	이렇게 쓰던애
+		//Integer result = new Integer(10) + new Integer(7);
+		Integer result = i01 + i02;	//둘다 같은 뜻.
+		
+		
 		
 		System.out.println(result);
 		

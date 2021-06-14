985ff75fa040ab987d94d7a60fbb5d3da651bf77
wonho <wonho951@gmail.com>
2021-06-14 10:16:10 +0900

    2021.06.14 수업내용 wrapper class

---
+++ src/com/javaex/ex06/Ex01.java
@@ -0,0 +1,19 @@
+package com.javaex.ex06;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		//wrapper class
+		Integer i01 = new Integer(10);	//--> Integer 곧 자바에서 없어질듯
+		Integer i02 = new Integer(7);
+		
+		Integer result = i01 + i02;
+		
+		System.out.println(result);
+		
+		
+		
+	}
+
+}
